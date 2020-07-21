1fbb82136ee57b30a2f4eaea27e2f61fa670ea71
Val De Anda <val@Vals-MBP.stx.rr.com>
2020-07-20 22:59:50 -0500

    script to remove sequences from a fasta file based on a list of headers to remove

---
+++ remove_sequences.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from Bio import SeqIO
+import sys
+
+ffile = SeqIO.parse(sys.argv[1], "fasta")
+header_set = set(line.strip() for line in open(sys.argv[2]))
+
+for seq_record in ffile:
+    try:
+        header_set.remove(seq_record.name)
+    except KeyError:
+        print(seq_record.format("fasta"))
+        continue
+if len(header_set) != 0:
+    print(len(header_set),'of the headers from list were not identified in the input fasta file.', file=sys.stderr)
